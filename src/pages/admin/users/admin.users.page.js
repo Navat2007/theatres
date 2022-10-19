48ef21ef2d033da04ffa5d83e2e589b7c60c4a56
Savallera <phenixvn1985@gmail.com>
2022-10-19 16:41:19 +0300

    добавить админа

--- src/pages/admin/users/admin.users.page.js
+++ src/pages/admin/users/admin.users.page.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import Button from "../../../components/simple/button/button.component";
 
 const AdminUsersPage = () => {
 
@@ -14,10 +15,39 @@ const AdminUsersPage = () => {
                 id && <p>Админ №{id}</p>
             } */}
 
+            <Button
+                className="button --icon-back --icon-on-before --theme-text"
+                type="button"
+                text="Назад"
+                aria-label="Назад"
+            />
             <h1 className="content__title">Создание администратора</h1>
             <form action="" className='form'>
                 <h2 className="form__title">Основная информация</h2>
-                
+                <div class="field">
+                    <label class="field__label">Email (логин)</label>
+                    <input
+                        type="text"
+                        class="field__input"
+                        placeholder="Введите email..."
+                        name=""
+                        required
+                    />
+                    <span class="field__icon --type-error"></span>
+                    <p class="field__info"></p>
+                </div>
+                <div class="field">
+                    <label class="field__label">Полное наименование организации</label>
+                    <input
+                        type="text"
+                        class="field__input"
+                        placeholder="Введите полное наименование организации..."
+                        name=""
+                        required
+                    />
+                    <span class="field__icon --type-error"></span>
+                    <p class="field__info"></p>
+                </div>
             </form>
         </div>
     );
