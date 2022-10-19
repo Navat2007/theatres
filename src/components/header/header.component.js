aafe463eac2468c410fc20e71116e053910cc2ba
Navat2007 <navat2007@yandex.ru>
2022-10-19 14:47:52 +0300

    add

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
 
 import ProfileHeader from "./profile.header.component";
 import Button from "../simple/button/button.component";
 
-import { logout } from "../../store/authSlice";
-
 const HeaderComponent = ({ children, handleBurger }) => {
-
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-
-    const handleLogout = () => {
-        if (window.confirm("Вы действительно хотите выйти?")) {
-            dispatch(logout());
-            navigate("/login", { replace: true });
-        }
-    }
 
     return (
         <header className="header content__header">
@@ -29,14 +15,6 @@ const HeaderComponent = ({ children, handleBurger }) => {
             />
             {children}
             <ProfileHeader className="--place-header" />
-            {/* <Button
-                className="header__item --type-exit"
-                type="button"
-                aria-label="Выйти из профиля"
-                onClick={handleLogout}
-            >
-                <span>Выход</span>
-            </Button> */}
         </header>
     );
 };
--- src/components/header/profile.header.component.js
+++ src/components/header/profile.header.component.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useNavigate} from "react-router-dom";
 
 import Button from "../simple/button/button.component";
 
 import no_photo_man from '../../images/no_photo_man.png';
+import {logout} from "../../store/authSlice";
 
 const ProfileHeader = ({className}) => {
 
     const {user} = useSelector(state => state.auth);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const [userRole, setUserRole] = React.useState("");
 
     React.useEffect(() => {
@@ -31,6 +33,13 @@ const ProfileHeader = ({className}) => {
 
     }, [user.role]);
 
+    const handleLogout = () => {
+        if (window.confirm("Вы действительно хотите выйти?")) {
+            dispatch(logout());
+            navigate("/login", { replace: true });
+        }
+    }
+
     return (
         <div className={`profile ${className}`} onClick={() => { navigate("/profile")}}>
             <img className="profile__img" src={user?.photo !== "" ? window.global.baseUrl + user.photo : no_photo_man} alt="Фото профиля" />
@@ -42,6 +51,7 @@ const ProfileHeader = ({className}) => {
                 className="profile__btn"
                 type="button"
                 aria-label="Выйти из профиля"
+                onClick={handleLogout}
             >
             </Button>
         </div>
