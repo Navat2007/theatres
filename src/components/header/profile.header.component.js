25c1ff3bd963c6a188e53dcfa7507ae55e51ef09
Savallera <phenixvn1985@gmail.com>
2022-10-21 11:15:33 +0300

    profile

--- src/components/header/profile.header.component.js
+++ src/components/header/profile.header.component.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {useNavigate} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import Button from "../simple/button/button.component";
 
 import no_photo_man from '../../images/no_photo_man.png';
-import {logout} from "../../store/authSlice";
+import { logout } from "../../store/authSlice";
 import Popup from "../popup/popup.component";
 
-const ProfileHeader = ({className}) => {
+const ProfileHeader = ({ className }) => {
 
-    const {user} = useSelector(state => state.auth);
+    const { user } = useSelector(state => state.auth);
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const [userRole, setUserRole] = React.useState("");
@@ -42,11 +42,9 @@ const ProfileHeader = ({className}) => {
                 navigate("/profile")
             }}>
                 <img className="profile__img"
-                     src={user?.photo !== "" ? window.global.baseUrl + user.photo : no_photo_man} alt="Фото профиля"/>
-                <div className="profile__info">
-                    <p className="profile__title">{user?.email}</p>
-                    <p className="profile__subtitle">{userRole}</p>
-                </div>
+                    src={user?.photo !== "" ? window.global.baseUrl + user.photo : no_photo_man} alt="Фото профиля" />
+                <p className="profile__title">{user?.email}</p>
+                <p className="profile__subtitle">{userRole}</p>
             </div>
             <Button
                 className="profile__btn"
@@ -68,7 +66,7 @@ const ProfileHeader = ({className}) => {
                         text={"Да"}
                         onClick={() => {
                             dispatch(logout());
-                            navigate("/login", {replace: true});
+                            navigate("/login", { replace: true });
                         }}
                     />
                     <Button
