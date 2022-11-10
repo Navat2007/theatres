1efbe6fb88198a8c041793ce36a7a74200359254
Navat2007 <navat2007@yandex.ru>
2022-11-11 00:11:15 +0300

    add

--- src/components/multi_select/multi_select.component.js
+++ src/components/multi_select/multi_select.component.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { motion, Variants } from 'framer-motion';
+import React from "react";
+import { motion, Variants } from "framer-motion";
 import { useForm } from "react-hook-form";
 
 import Button from "../simple/button/button.component";
 
 const MultiSelect = ({ list, multi = false, ...rest }) => {
-
-    const { register, handleSubmit, reset, getValues, setValue, watch } = useForm();
+    const { register, handleSubmit, reset, getValues, setValue, watch } =
+        useForm();
 
     const [opened, setOpened] = React.useState(false);
     const [selectedItems, setSelectedItems] = React.useState([]);
@@ -14,125 +14,220 @@ const MultiSelect = ({ list, multi = false, ...rest }) => {
 
     const searchInput = watch("search");
 
-    const onSubmit = handleSubmit(data => {
+    const onSubmit = handleSubmit((data) => {
         console.log(data);
     });
 
-    React.useEffect(() => {
-
-        if (searchInput && !opened)
-            setOpened(!opened);
-
-    }, [searchInput]);
-
     return (
         <form onSubmit={onSubmit}>
-            <div className="field --type-multiselect">
-                {
-                    selectedItems && multi && selectedItems.map((item, index) =>
-                        <div key={item.value + '_' + index} className="field__chip">
-                            <p className='field__chip-text'>{item.label}</p>
-                            <span className="field__chip-icon" aria-label='Удалить' onClick={() => {
-                                setValue('check_' + item.value, !getValues('check_' + item.value));
-                                const temp = [...selectedItems];
-                                temp.splice(index, 1);
-                                setSelectedItems(temp);
-                            }} />
-                        </div>)
-                }
+            <div
+                className="field --type-multiselect"
+                onClick={() => {
+                    if (!opened)
+                        setOpened(!opened)
+                }}
+            >
+                {selectedItems &&
+                    multi &&
+                    selectedItems.map((item, index) => (
+                        <div
+                            key={item.value + "_" + index}
+                            className="field__chip"
+                        >
+                            <p className="field__chip-text">{item.label}</p>
+                            <span
+                                className="field__chip-icon"
+                                aria-label="Удалить"
+                                onClick={() => {
+                                    setValue(
+                                        "check_" + item.value,
+                                        !getValues("check_" + item.value)
+                                    );
+                                    const temp = [...selectedItems];
+                                    temp.splice(index, 1);
+                                    setSelectedItems(temp);
+                                }}
+                            />
+                        </div>
+                    ))}
                 <input
-                    className='field__chip-input'
+                    className="field__chip-input"
                     type="text"
-                    placeholder='Выбрать или найти...'
+                    placeholder="Выбрать или найти..."
                     onKeyUp={(value) => setSearch(value)}
                     {...register("search")}
                 />
-                <select
-                    multiple
-                    className="--hide"
-                    {...register("select")}
-                >
-                    {
-                        selectedItems && selectedItems.map((item, index) =>
-                            <option key={item.value + '_' + index} value={item.value}>
+                <select multiple className="--hide" {...register("select")}>
+                    {selectedItems &&
+                        selectedItems.map((item, index) => (
+                            <option
+                                key={item.value + "_" + index}
+                                value={item.value}
+                            >
                                 {item.label}
-                            </option>)
-                    }
+                            </option>
+                        ))}
                 </select>
                 <motion.span
                     animate={opened ? "open" : "closed"}
                     variants={{
                         open: { rotateX: 180 },
-                        closed: { rotateX: 0 }
+                        closed: { rotateX: 0 },
                     }}
                     transition={{ duration: 0.2 }}
                     className="field__icon --type-dropdown"
-                    onClick={() => setOpened(!opened)}
+                    onClick={() => { setOpened(!opened) }}
                 />
-                {searchInput && <span className="field__icon --type-delete" aria-label='Очистить строку' onClick={() => setValue("search", "")} />}
+                {searchInput && (
+                    <span
+                        className="field__icon --type-delete"
+                        aria-label="Очистить строку"
+                        onClick={() => setValue("search", "")}
+                    />
+                )}
                 {/* Стандартная иконка для отображения ошибки, отображается при установленном классе --state-error (у field) */}
                 <span className="field__icon --type-error" />
                 {/* Стандартное полу для вывода текста ошибки*/}
                 <p className="field__info">Сообщение об ошибке</p>
 
-                <div className={`field__list-container${opened ? ' --opened' : ''}`}>
-                    <ul className='field__list'>
-                        {
-                            list && multi && list.length > 0 &&
+                <div
+                    className={`field__list-container${opened ? " --opened" : ""
+                        }`}
+                >
+                    <ul className="field__list">
+                        {list &&
+                            multi &&
+                            list.length > 0 &&
                             list
-                                .filter(item => item.label.toLowerCase().includes(searchInput ? searchInput.toLowerCase() : ""))
-                                .map((item, index) =>
+                                .filter((item) =>
+                                    item.label
+                                        .toLowerCase()
+                                        .includes(
+                                            searchInput
+                                                ? searchInput.toLowerCase()
+                                                : ""
+                                        )
+                                )
+                                .map((item, index) => (
                                     <li
-                                        key={item.value + '_' + index}
-                                        className='field__item'
+                                        key={item.value + "_" + index}
+                                        className="field__item"
                                         style={{ cursor: "pointer" }}
                                         onClick={() => {
-                                            setValue('check_' + item.value, !getValues('check_' + item.value));
-                                            if (getValues('check_' + item.value)) {
-                                                setSelectedItems(prevArray => [...prevArray, { label: item.label, value: item.value }]);
-                                            }
-                                            else {
+                                            setValue(
+                                                "check_" + item.value,
+                                                !getValues(
+                                                    "check_" + item.value
+                                                )
+                                            );
+                                            if (
+                                                getValues("check_" + item.value)
+                                            ) {
+                                                setSelectedItems(
+                                                    (prevArray) => [
+                                                        ...prevArray,
+                                                        {
+                                                            label: item.label,
+                                                            value: item.value,
+                                                        },
+                                                    ]
+                                                );
+                                            } else {
                                                 const temp = [...selectedItems];
                                                 temp.splice(index, 1);
                                                 setSelectedItems(temp);
                                             }
                                         }}
                                     >
-                                        <div className="field --type-checkbox" style={{ pointerEvents: "none" }}>
-                                            <input className='field__checkbox' type="checkbox" id={'check_' + item.value} {...register('check_' + item.value)} />
-                                            <label className='field__label' htmlFor={'check_' + item.value}>{item.label}</label>
+                                        <div
+                                            className="field --type-checkbox"
+                                            style={{ pointerEvents: "none" }}
+                                        >
+                                            <input
+                                                className="field__checkbox"
+                                                type="checkbox"
+                                                id={"check_" + item.value}
+                                                {...register(
+                                                    "check_" + item.value
+                                                )}
+                                            />
+                                            <label
+                                                className="field__label"
+                                                htmlFor={"check_" + item.value}
+                                            >
+                                                {item.label}
+                                            </label>
                                         </div>
-                                    </li>)
-                        }
-                        {
-                            (!list || list.filter(item => item.label.toLowerCase().includes(searchInput ? searchInput.toLowerCase() : "")).length === 0)
-                            &&
-                            <li className='field__item'><p>Нет опций для отображения</p></li>
-                        }
+                                    </li>
+                                ))}
+                        {list && !multi && list.length > 0 &&
+                            list
+                                .filter((item) =>
+                                    item.label
+                                        .toLowerCase()
+                                        .includes(
+                                            searchInput
+                                                ? searchInput.toLowerCase()
+                                                : ""
+                                        )
+                                )
+                                .map((item, index) => (
+                                    <li
+                                        key={item.value + "_" + index}
+                                        className="field__item"
+                                        style={{ cursor: "pointer" }}
+                                        onClick={() => {
+                                            setValue("search", item.label);
+                                            setOpened(!opened);
+                                        }}
+                                    >
+                                        <div
+                                            className="field"
+                                            style={{ pointerEvents: "none" }}
+                                        >
+                                            <p>{item.label}</p>
+                                        </div>
+                                    </li>
+                                ))}
+                        {(!list ||
+                            list.filter((item) =>
+                                item.label
+                                    .toLowerCase()
+                                    .includes(
+                                        searchInput
+                                            ? searchInput.toLowerCase()
+                                            : ""
+                                    )
+                            ).length === 0) && (
+                                <li className="field__item">
+                                    <p>Нет опций для отображения</p>
+                                </li>
+                            )}
                     </ul>
-                    {list && <div className="field__list-controls">
-                        <Button
-                            type='button'
-                            size='small'
-                            text={'Готово'}
-                            onClick={() => setOpened(!opened)}
-                        />
-                        <Button
-                            type='button'
-                            size='small'
-                            theme='text'
-                            text={'Очистить'}
-                            onClick={() => {
-                                reset();
-                                setSelectedItems([]);
-                            }}
-                        />
-                    </div>}
+                    {list && (
+                        <div className="field__list-controls">
+                            <Button
+                                type="button"
+                                size="small"
+                                text={"Готово"}
+                                onClick={() => setOpened(!opened)}
+                            />
+                            <Button
+                                type="button"
+                                size="small"
+                                theme="text"
+                                text={"Очистить"}
+                                onClick={() => {
+                                    reset();
+                                    setSelectedItems([]);
+                                }}
+                            />
+                        </div>
+                    )}
                 </div>
             </div>
-        </form>
+        </form >
     );
-
 };
 
 export default MultiSelect;
--- src/pages/user/theatres/my.theatre.page.js
+++ src/pages/user/theatres/my.theatre.page.js
@@ -1,70 +1,57 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 import MultiSelect from "../../../components/multi_select/multi_select.component";
 
-import { clear, loadTeachers } from '../../../store/admin/teachersSlice';
-import { loadSchools } from '../../../store/admin/schoolsSlice';
+import { clear, loadTeachers } from "../../../store/admin/teachersSlice";
+import { loadSchools } from "../../../store/admin/schoolsSlice";
 
 const MyTheatrePage = () => {
-
     let { id } = useParams();
     const dispatch = useDispatch();
 
-    const user = useSelector(state => state.auth.user);
-    const { data, statusError } = useSelector(state => state.teachers);
+    const user = useSelector((state) => state.auth.user);
+    const { data, statusError } = useSelector((state) => state.teachers);
 
     const { register, handleSubmit, reset } = useForm();
 
-    const school = useSelector(state => state.schools);
+    const school = useSelector((state) => state.schools);
 
     const fetchData = async () => {
-
-        await dispatch(loadTeachers({schoolID: user.schoolID}));       
-        await dispatch(loadSchools()); 
-
-    }
+        await dispatch(loadTeachers({ schoolID: user.schoolID }));
+        await dispatch(loadSchools());
+    };
 
     React.useEffect(() => {
-
-        fetchData();   
+        fetchData();
 
         return () => {
             dispatch(clear());
         };
-
     }, [id]);
 
-    React.useEffect(() => {
-
-        
-
-    }, []);
-
-    console.log("From redux: ", data);
-    console.log("From redux: ", school);
+    React.useEffect(() => {}, []);
 
     return (
-        <div className='content__section'>
-            {
-                !id
-                &&
+        <div className="content__section">
+            {!id && (
                 <>
-                    <h1 className='content__title'>Новый театр</h1>
-                    <MultiSelect multi={true} list={school.data.map(item => {
-                        return {
-                            //label: `${item.f} ${item.i} ${item.o}`,
-                            label: `${item.org_short_name}`,
-                            value: item.ID
-                        }
-                    })} />
+                    <h1 className="content__title">Новый театр</h1>
+                    <MultiSelect
+                        multi={false}
+                        list={school.data.map((item) => {
+                            return {
+                                //label: `${item.f} ${item.i} ${item.o}`,
+                                label: `${item.org_short_name}`,
+                                value: item.ID,
+                            };
+                        })}
+                    />
                 </>
-            }
-            {
-                id && <p>Театр №{id}</p>
-            }
+            )}
+            {id && <p>Театр №{id}</p>}
         </div>
     );
 };
