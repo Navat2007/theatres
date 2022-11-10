35f21f10b0296ab9dae0f8be01b0499570353417
Navat2007 <navat2007@yandex.ru>
2022-11-10 15:58:10 +0300

    add

--- src/components/multi_select/multi_select.component.js
+++ src/components/multi_select/multi_select.component.js
@@ -1,57 +1,51 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
 import Button from "../simple/button/button.component";
 
-const MultiSelect = ({list, multi = false}) => {
+const MultiSelect = ({list, multi = false, ...rest}) => {
 
-
+    const [opened, setOpened] = React.useState(false);
+    const [selectedItems, setSelectedItems] = React.useState([]);
+    const [search, setSearch] = React.useState("");
 
     return (
         <div className="field --type-multiselect">
-            <div className="field__chip">
-                <p className='field__chip-text'>Иванова Татьяна Александровна</p>
-                <span className="field__chip-icon" aria-label='Удалить'></span>
-            </div>
-            <div className="field__chip">
-                <p className='field__chip-text'>Грузинов Борис Иванович</p>
-                <span className="field__chip-icon" aria-label='Удалить'></span>
-            </div>
-            <div className="field__chip">
-                <p className='field__chip-text'>Шапошников Иван Петрович</p>
-                <span className="field__chip-icon" aria-label='Удалить'></span>
-            </div>
-            <div className="field__chip">
-                <p className='field__chip-text'>Шапошников Иван Петрович</p>
-                <span className="field__chip-icon" aria-label='Удалить'></span>
-            </div>
-            <div className="field__chip">
-                <p className='field__chip-text'>Шапошников Иван Петрович</p>
-                <span className="field__chip-icon" aria-label='Удалить'></span>
-            </div>
+            {
+                selectedItems && selectedItems.map((item, index) =>
+                    <div key={item.value + '_' + index}>
+                        <p className='field__chip-text'>{item.value}</p>
+                        <span className="field__chip-icon" aria-label='Удалить'/>
+                    </div>)
+            }
             {/* Текст.поле может быть разных типов, если мультиселект используем text, чтобы можно было ввести значения */}
-            <input className='field__chip-input' type="text" placeholder='Выбрать или найти...'/>
+            <input className='field__chip-input' type="text" placeholder='Выбрать или найти...' />
             {/* Стрелка для селекта, при нажатии тоглится вып.список, для всех списков необходимо добавлять */}
-            <span className="field__icon --type-dropdown"></span>
+            <span className="field__icon --type-dropdown" onClick={() => setOpened(!opened)} />
             {/* Крестик для очистки интупа, нужен только если идет поиск по строке.
                         Можно также добавить для  все интупов текста, например */}
-            <span className="field__icon --type-delete" aria-label='Очистить строку'></span>
+            {search && <span className="field__icon --type-delete" aria-label='Очистить строку'/>}
             {/* Стандартная иконка для отображения ошибки, отображается при установленном классе --state-error (у field), не всегда нужно бывает, если нет валидации, то нафиг */}
-            <span className="field__icon --type-error"></span>
+            <span className="field__icon --type-error"/>
             {/* Стандартное полу для вывода текста ошибки, не всегда нужно бывает, если нет валидации, то нафиг */}
             <p className="field__info">Сообщение об ошибке</p>
             {/* Для открытия/закрытия выпадающего списка тоглим --opened */}
-            <div className="field__list-container --opened">
+            <div className={`field__list-container${opened ? ' --opened': ''}`}>
                 <ul className='field__list'>
                     {
                         list && multi && list.map((item, index) =>
-                            <li key={item.value  + '_' + index} className='field__item'>
+                            <li key={item.value + '_' + index} className='field__item'>
                                 <div className="field --type-checkbox">
                                     <input className='field__checkbox' type="checkbox" id={'check_' + index}/>
                                     <label className='field__label' htmlFor={'check_' + index}>{item.label}</label>
                                 </div>
                             </li>)
                     }
+                    {
+                        !list && <li className='field__item'><p>Нет опций для отображения</p></li>
+                    }
                 </ul>
-                <div className="field__list-controls">
+                {list && <div className="field__list-controls">
                     <Button
                         type='button'
                         size='small'
@@ -61,8 +55,9 @@ const MultiSelect = ({list, multi = false}) => {
                         size='small'
                         theme='text'
                         text={'Очистить'}/>
-                </div>
+                </div>}
             </div>
+            <select className="--hide"></select>
         </div>
     );
 
