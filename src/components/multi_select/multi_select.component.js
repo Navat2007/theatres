a7fdb74f7e6b5c968ad402b425e71977f1169f2e
Navat2007 <navat2007@yandex.ru>
2022-11-10 16:56:42 +0300

    add

--- src/components/multi_select/multi_select.component.js
+++ src/components/multi_select/multi_select.component.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
 
 import Button from "../simple/button/button.component";
 
-const MultiSelect = ({list, multi = false, ...rest}) => {
+const MultiSelect = ({ list, multi = false, ...rest }) => {
 
     const [opened, setOpened] = React.useState(false);
     const [selectedItems, setSelectedItems] = React.useState([]);
     const [search, setSearch] = React.useState("");
+
+    const itemVariants: Variants = {
+        open: {
+          opacity: 1,
+          y: 0,
+          transition: { type: "spring", stiffness: 300, damping: 24 }
+        },
+        closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
+      };
 
     return (
         <div className="field --type-multiselect">
@@ -15,28 +24,34 @@ const MultiSelect = ({list, multi = false, ...rest}) => {
                 selectedItems && selectedItems.map((item, index) =>
                     <div key={item.value + '_' + index}>
                         <p className='field__chip-text'>{item.value}</p>
-                        <span className="field__chip-icon" aria-label='Удалить'/>
+                        <span className="field__chip-icon" aria-label='Удалить' />
                     </div>)
             }
-            {/* Текст.поле может быть разных типов, если мультиселект используем text, чтобы можно было ввести значения */}
-            <input className='field__chip-input' type="text" placeholder='Выбрать или найти...' />
-            {/* Стрелка для селекта, при нажатии тоглится вып.список, для всех списков необходимо добавлять */}
-            <span className="field__icon --type-dropdown" onClick={() => setOpened(!opened)} />
-            {/* Крестик для очистки интупа, нужен только если идет поиск по строке.
-                        Можно также добавить для  все интупов текста, например */}
-            {search && <span className="field__icon --type-delete" aria-label='Очистить строку'/>}
+            <input className='field__chip-input' type="text" placeholder='Выбрать или найти...' onKeyUp={(value) => setSearch(value)} />
+            
+            <motion.span
+                animate={opened ? "open" : "closed"}
+                variants={{
+                    open: { rotateX: 180 },
+                    closed: { rotateX: 0 }
+                }}
+                transition={{ duration: 0.2 }}
+                className="field__icon --type-dropdown"
+                onClick={() => setOpened(!opened)}
+            />            
+            {search && <span className="field__icon --type-delete" aria-label='Очистить строку' onClick={() => setSearch("")} />}
             {/* Стандартная иконка для отображения ошибки, отображается при установленном классе --state-error (у field), не всегда нужно бывает, если нет валидации, то нафиг */}
-            <span className="field__icon --type-error"/>
+            <span className="field__icon --type-error" />
             {/* Стандартное полу для вывода текста ошибки, не всегда нужно бывает, если нет валидации, то нафиг */}
             <p className="field__info">Сообщение об ошибке</p>
             {/* Для открытия/закрытия выпадающего списка тоглим --opened */}
-            <div className={`field__list-container${opened ? ' --opened': ''}`}>
+            <div className={`field__list-container${opened ? ' --opened' : ''}`}>
                 <ul className='field__list'>
                     {
                         list && multi && list.map((item, index) =>
                             <li key={item.value + '_' + index} className='field__item'>
                                 <div className="field --type-checkbox">
-                                    <input className='field__checkbox' type="checkbox" id={'check_' + index}/>
+                                    <input className='field__checkbox' type="checkbox" id={'check_' + index} />
                                     <label className='field__label' htmlFor={'check_' + index}>{item.label}</label>
                                 </div>
                             </li>)
@@ -49,12 +64,12 @@ const MultiSelect = ({list, multi = false, ...rest}) => {
                     <Button
                         type='button'
                         size='small'
-                        text={'Готово'}/>
+                        text={'Готово'} />
                     <Button
                         type='button'
                         size='small'
                         theme='text'
-                        text={'Очистить'}/>
+                        text={'Очистить'} />
                 </div>}
             </div>
             <select className="--hide"></select>
--- src/pages/admin/theatres/theatre.page.js
+++ src/pages/admin/theatres/theatre.page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import Button from "../../../components/simple/button/button.component";
+
 import MultiSelect from "../../../components/multi_select/multi_select.component";
 
 const TheatrePage = () => {
@@ -10,9 +10,11 @@ const TheatrePage = () => {
     return (
         <div className='content__section'>
             {
-                !id && <>
-                    <h1 className='content__title'>Новой театр</h1>
-                    <MultiSelect  />
+                !id
+                &&
+                <>
+                    <h1 className='content__title'>Новый театр</h1>
+                    <MultiSelect />
                 </>
             }
             {
