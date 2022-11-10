1edac16187aa784873e377e0f2d70355bb9efcf7
Navat2007 <navat2007@yandex.ru>
2022-11-10 19:19:03 +0300

    add

--- src/components/multi_select/multi_select.component.js
+++ src/components/multi_select/multi_select.component.js
@@ -1,79 +1,113 @@
 import React from 'react';
 import { motion, Variants } from 'framer-motion';
+import { useForm } from "react-hook-form";
 
 import Button from "../simple/button/button.component";
 
 const MultiSelect = ({ list, multi = false, ...rest }) => {
 
+    const { register, handleSubmit, reset, getValues, setValue, watch } = useForm();
+
     const [opened, setOpened] = React.useState(false);
     const [selectedItems, setSelectedItems] = React.useState([]);
     const [search, setSearch] = React.useState("");
 
-    const itemVariants: Variants = {
-        open: {
-          opacity: 1,
-          y: 0,
-          transition: { type: "spring", stiffness: 300, damping: 24 }
-        },
-        closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
-      };
+    const searchInput = watch("search");
+
+    const onSubmit = handleSubmit(data => {
+        console.log(data);
+    });
+
+    React.useEffect(() => {
+
+        if(searchInput && !opened)
+            setOpened(!opened);
+
+    }, [searchInput]);
 
     return (
-        <div className="field --type-multiselect">
-            {
-                selectedItems && selectedItems.map((item, index) =>
-                    <div key={item.value + '_' + index}>
-                        <p className='field__chip-text'>{item.value}</p>
-                        <span className="field__chip-icon" aria-label='Удалить' />
-                    </div>)
-            }
-            <input className='field__chip-input' type="text" placeholder='Выбрать или найти...' onKeyUp={(value) => setSearch(value)} />
-            
-            <motion.span
-                animate={opened ? "open" : "closed"}
-                variants={{
-                    open: { rotateX: 180 },
-                    closed: { rotateX: 0 }
-                }}
-                transition={{ duration: 0.2 }}
-                className="field__icon --type-dropdown"
-                onClick={() => setOpened(!opened)}
-            />            
-            {search && <span className="field__icon --type-delete" aria-label='Очистить строку' onClick={() => setSearch("")} />}
-            {/* Стандартная иконка для отображения ошибки, отображается при установленном классе --state-error (у field), не всегда нужно бывает, если нет валидации, то нафиг */}
-            <span className="field__icon --type-error" />
-            {/* Стандартное полу для вывода текста ошибки, не всегда нужно бывает, если нет валидации, то нафиг */}
-            <p className="field__info">Сообщение об ошибке</p>
-            {/* Для открытия/закрытия выпадающего списка тоглим --opened */}
-            <div className={`field__list-container${opened ? ' --opened' : ''}`}>
-                <ul className='field__list'>
+        <form onSubmit={onSubmit}>
+            <div className="field --type-multiselect">
+                {
+                    selectedItems && multi && selectedItems.map((item, index) =>
+                        <div key={item.value + '_' + index}>
+                            <p className='field__chip-text'>{item.value}</p>
+                            <span className="field__chip-icon" aria-label='Удалить' />
+                        </div>)
+                }
+                <input
+                    className='field__chip-input'
+                    type="text"
+                    placeholder='Выбрать или найти...'
+                    onKeyUp={(value) => setSearch(value)}
+                    {...register("search")}
+                />
+                <select
+                    multiple
+                    className="--hide"
+                    {...register("select")}
+                >
                     {
-                        list && multi && list.map((item, index) =>
-                            <li key={item.value + '_' + index} className='field__item'>
-                                <div className="field --type-checkbox">
-                                    <input className='field__checkbox' type="checkbox" id={'check_' + index} />
-                                    <label className='field__label' htmlFor={'check_' + index}>{item.label}</label>
-                                </div>
-                            </li>)
+                        list && list.map((item, index) =>
+                            <option key={item.value + '_' + index} value={item.value}>
+                                {item.label}
+                            </option>)
                     }
-                    {
-                        !list && <li className='field__item'><p>Нет опций для отображения</p></li>
-                    }
-                </ul>
-                {list && <div className="field__list-controls">
-                    <Button
-                        type='button'
-                        size='small'
-                        text={'Готово'} />
-                    <Button
-                        type='button'
-                        size='small'
-                        theme='text'
-                        text={'Очистить'} />
-                </div>}
+                </select>
+                <motion.span
+                    animate={opened ? "open" : "closed"}
+                    variants={{
+                        open: { rotateX: 180 },
+                        closed: { rotateX: 0 }
+                    }}
+                    transition={{ duration: 0.2 }}
+                    className="field__icon --type-dropdown"
+                    onClick={() => setOpened(!opened)}
+                />
+                {searchInput && <span className="field__icon --type-delete" aria-label='Очистить строку' onClick={() => setValue("search", "")} />}
+                {/* Стандартная иконка для отображения ошибки, отображается при установленном классе --state-error (у field) */}
+                <span className="field__icon --type-error" />
+                {/* Стандартное полу для вывода текста ошибки*/}
+                <p className="field__info">Сообщение об ошибке</p>
+
+                <div className={`field__list-container${opened ? ' --opened' : ''}`}>
+                    <ul className='field__list'>
+                        {
+                            list && multi && list.length > 0 &&
+                            list
+                                .filter(item => item.label.toLowerCase().includes(searchInput ? searchInput.toLowerCase() : ""))
+                                .map((item, index) =>
+                                    <li key={item.value + '_' + index} className='field__item'>
+                                        <div className="field --type-checkbox">
+                                            <input className='field__checkbox' type="checkbox" id={'check_' + index} />
+                                            <label className='field__label' htmlFor={'check_' + index}>{item.label}</label>
+                                        </div>
+                                    </li>)
+                        }
+                        {
+                            (!list || list.filter(item => item.label.toLowerCase().includes(searchInput ? searchInput.toLowerCase() : "")).length === 0)
+                            &&
+                            <li className='field__item'><p>Нет опций для отображения</p></li>
+                        }
+                    </ul>
+                    {list && <div className="field__list-controls">
+                        <Button
+                            type='button'
+                            size='small'
+                            text={'Готово'}
+                            onClick={() => setOpened(!opened)}
+                        />
+                        <Button
+                            type='button'
+                            size='small'
+                            theme='text'
+                            text={'Очистить'}
+                            onClick={() => reset()}
+                        />
+                    </div>}
+                </div>
             </div>
-            <select className="--hide"></select>
-        </div>
+        </form>
     );
 
 };
--- src/pages/user/theatres/my.theatre.page.js
+++ src/pages/user/theatres/my.theatre.page.js
@@ -1,14 +1,60 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+
+import MultiSelect from "../../../components/multi_select/multi_select.component";
+
+import { clear, loadTeachers } from '../../../store/admin/teachersSlice';
 
 const MyTheatrePage = () => {
 
     let { id } = useParams();
+    const dispatch = useDispatch();
+
+    const user = useSelector(state => state.auth.user);
+    const { data, statusError } = useSelector(state => state.teachers);
+
+    const { register, handleSubmit, reset } = useForm();
+
+    const fetchData = async () => {
+
+        await dispatch(loadTeachers({schoolID: user.schoolID}));        
+
+    }
+
+    React.useEffect(() => {
+
+        fetchData();   
+
+        return () => {
+            dispatch(clear());
+        };
+
+    }, [id]);
+
+    React.useEffect(() => {
+
+        
+
+    }, []);
+
+    console.log("From redux: ", data);
 
     return (
         <div className='content__section'>
             {
-                !id && <p>Новой театр</p>
+                !id
+                &&
+                <>
+                    <h1 className='content__title'>Новый театр</h1>
+                    <MultiSelect multi={true} list={data.map(item => {
+                        return {
+                            label: `${item.f} ${item.i} ${item.o}`,
+                            value: item.ID
+                        }
+                    })} />
+                </>
             }
             {
                 id && <p>Театр №{id}</p>
