5bad9ff429993b6f094246d3f01ecd2de8f6e8c8
Navat2007 <navat2007@yandex.ru>
2022-11-11 12:58:05 +0300

    add

--- src/pages/user/theatres/my.theatre.page.js
+++ src/pages/user/theatres/my.theatre.page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
+import ReactSelect from 'react-select'
 
 import MultiSelect from "../../../components/multi_select/multi_select.component";
 
 import { clear, loadTeachers } from "../../../store/admin/teachersSlice";
 import { loadSchools } from "../../../store/admin/schoolsSlice";
+import Button from "../../../components/simple/button/button.component";
 
 const MyTheatrePage = () => {
     let { id } = useParams();
@@ -15,7 +17,7 @@ const MyTheatrePage = () => {
     const user = useSelector((state) => state.auth.user);
     const { data, statusError } = useSelector((state) => state.teachers);
 
-    const { register, handleSubmit, reset } = useForm();
+    const { register, handleSubmit, reset, control } = useForm();
 
     const school = useSelector((state) => state.schools);
 
@@ -34,35 +36,67 @@ const MyTheatrePage = () => {
 
     React.useEffect(() => { }, []);
 
+    const onSubmit = handleSubmit((data) => {
+        console.log(data);
+    });
+
+    console.log("render theatre");
+
     return (
         <div className="content__section">
             {!id && (
                 <>
                     <h1 className="content__title">Новый театр</h1>
-                    <p>Single select</p>
-                    <MultiSelect
-                        multi={false}
-                        list={school.data.map((item) => {
-                            return {
-                                //label: `${item.f} ${item.i} ${item.o}`,
-                                label: `${item.org_short_name}`,
-                                value: item.ID,
-                            };
-                        })}
-                    />
-                    <br />
-                    <br />
-                    <p>Multi select</p>
-                    <MultiSelect
-                        multi={true}
-                        list={school.data.map((item) => {
-                            return {
-                                //label: `${item.f} ${item.i} ${item.o}`,
-                                label: `${item.org_short_name}`,
-                                value: item.ID,
-                            };
-                        })}
-                    />
+                    <form onSubmit={onSubmit}>
+                        <p>Single select</p>
+                        <MultiSelect
+                            multi={false}
+                            list={school.data.map((item) => {
+                                return {
+                                    //label: `${item.f} ${item.i} ${item.o}`,
+                                    label: `${item.org_short_name}`,
+                                    value: item.ID,
+                                };
+                            })}
+                        />
+                        <br />
+                        <br />
+                        <p>Multi select</p>
+                        <MultiSelect
+                            multi={true}
+                            list={school.data.map((item) => {
+                                return {
+                                    //label: `${item.f} ${item.i} ${item.o}`,
+                                    label: `${item.org_short_name}`,
+                                    value: item.ID,
+                                };
+                            })}
+                        />
+                        <br />
+                        <br />
+                        <p>ReactSelect</p>
+                        <Controller
+                            control={control}
+                            name="select"
+                            defaultValue={[]}
+                            render={({ field }) => (
+                                <ReactSelect
+                                    {...field}
+                                    isMulti
+                                    options={school.data.map((item) => {
+                                        return {
+                                            //label: `${item.f} ${item.i} ${item.o}`,
+                                            label: `${item.org_short_name}`,
+                                            value: item.ID,
+                                        };
+                                    })}
+                                />
+                            )}
+                        />
+                        <br />
+                        <br />
+                        <Button type="submit" theme="primary" text="Отправить" />
+                    </form>
                 </>
             )}
             {id && <p>Театр №{id}</p>}
