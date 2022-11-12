c277493ad2774dc17fee536d137c58a2dcca8433
Navat2007 <navat2007@yandex.ru>
2022-11-12 23:11:10 +0300

    add

--- src/components/routes.list.component.js
+++ src/components/routes.list.component.js
@@ -1,34 +1,32 @@
-import React, {lazy, Suspense} from 'react';
+import React from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
 import {useSelector} from "react-redux";
 
 import AdminLayout from "./layout/admin.layout.component";
 import UserLayout from "./layout/user.layout.component";
-import Preloader from "./preloader/preloader.component";
 
-const Page404 = lazy(() => import("../pages/404.page"));
-const LoginPage = lazy(() => import("../pages/login/login.page"));
+import Page404 from "../pages/404.page";
+import LoginPage from "../pages/login/login.page";
+import UsersPage from "../pages/admin/users/users.page";
+import AdminUsersPage from "../pages/admin/users/admin.users.page";
+import UserUsersPage from "../pages/admin/users/user.users.page";
+import MySchoolPage from "../pages/user/my.school.page";
+import TheatresPage from "../pages/admin/theatres/theatres.page";
+import TheatrePage from "../pages/admin/theatres/theatre.page";
+import ProfilePage from "../pages/profile.page";
+import SchoolsPage from "../pages/admin/schools/schools.page";
+import SchoolPage from "../pages/admin/schools/school.page";
+import TheatreRequestsPage from "../pages/admin/theatreRequests/theatreRequests.page";
+import TheatreRequestPage from "../pages/admin/theatreRequests/theatreRequest.page";
+import AdminTeachersPage from "../pages/admin/teachers/teachers.page";
+import AdminTeacherPage from "../pages/admin/teachers/teacher.page";
 
-const UsersPage = lazy(() => import("../pages/admin/users/users.page"));
-const AdminUsersPage = lazy(() => import("../pages/admin/users/admin.users.page"));
-const UserUsersPage = lazy(() => import("../pages/admin/users/user.users.page"));
-const MySchoolPage = lazy(() => import("../pages/user/my.school.page"));
-const TheatresPage = lazy(() => import("../pages/admin/theatres/theatres.page"));
-const TheatrePage = lazy(() => import("../pages/admin/theatres/theatre.page"));
-const ProfilePage = lazy(() => import("../pages/profile.page"));
-const SchoolsPage = lazy(() => import("../pages/admin/schools/schools.page"));
-const SchoolPage = lazy(() => import("../pages/admin/schools/school.page"));
-const TheatreRequestsPage = lazy(() => import("../pages/admin/theatreRequests/theatreRequests.page"));
-const TheatreRequestPage = lazy(() => import("../pages/admin/theatreRequests/theatreRequest.page"));
-const AdminTeachersPage = lazy(() => import("../pages/admin/teachers/teachers.page"));
-const AdminTeacherPage = lazy(() => import("../pages/admin/teachers/teacher.page"));
-
-const MyTheatresPage = lazy(() => import("../pages/user/theatres/my.theatres.page"));
-const MyTheatrePage = lazy(() => import("../pages/user/theatres/my.theatre.page"));
-const MyTheatreRequestsPage = lazy(() => import("../pages/user/theatreRequests/my.theatreRequests.page"));
-const MyTheatreRequestPage = lazy(() => import("../pages/user/theatreRequests/my.theatreRequest.page"));
-const UserTeachersPage = lazy(() => import("../pages/user/teachers/teachers.page"));
-const UserTeacherPage = lazy(() => import("../pages/user/teachers/teacher.page"));
+import MyTheatresPage from "../pages/user/theatres/my.theatres.page";
+import MyTheatrePage from "../pages/user/theatres/my.theatre.page";
+import MyTheatreRequestsPage from "../pages/user/theatreRequests/my.theatreRequests.page";
+import MyTheatreRequestPage from "../pages/user/theatreRequests/my.theatreRequest.page";
+import UserTeachersPage from "../pages/user/teachers/teachers.page";
+import UserTeacherPage from "../pages/user/teachers/teacher.page";
 
 const RoutesList = () => {
 
@@ -37,48 +35,43 @@ const RoutesList = () => {
     if (user && (user.role === "admin" || user.role === "superadmin")) {
 
         return (
-            <Suspense fallback={<Preloader/>}>
-                <Routes>
-                    <Route path="/admin" element={<AdminLayout/>}>
-                        {
-
-                        }
-                        <Route path="users">
-                            <Route index element={<UsersPage/>}/>
-                            <Route path="admin/:id" element={<AdminUsersPage/>}/>
-                            <Route path="admin/new" element={<AdminUsersPage/>}/>
-                            <Route path="user/:id" element={<UserUsersPage/>}/>
-                            <Route path="user/new" element={<UserUsersPage/>}/>
-                        </Route>
-                        <Route path="schools">
-                            <Route index element={<SchoolsPage/>}/>
-                            <Route path=":id" element={<SchoolPage/>}/>
-                            <Route path="new" element={<SchoolPage/>}/>
-                        </Route>
-                        <Route path="theatres">
-                            <Route index element={<TheatresPage/>}/>
-                            <Route path=":id" element={<TheatrePage/>}/>
-                            <Route path="new" element={<TheatrePage/>}/>
-                        </Route>
-                        <Route path="theatreRequests">
-                            <Route index element={<TheatreRequestsPage/>}/>
-                            <Route path=":id" element={<TheatreRequestPage/>}/>
-                            <Route path="new" element={<TheatreRequestPage/>}/>
-                        </Route>
-                        <Route path="teachers">
-                            <Route index element={<AdminTeachersPage/>}/>
-                            <Route path=":id" element={<AdminTeacherPage/>}/>
-                            <Route path="new" element={<AdminTeacherPage/>}/>
-                        </Route>
+            <Routes>
+                <Route path="/admin" element={<AdminLayout/>}>
+                    <Route path="users">
+                        <Route index element={<UsersPage/>}/>
+                        <Route path="admin/:id" element={<AdminUsersPage/>}/>
+                        <Route path="admin/new" element={<AdminUsersPage/>}/>
+                        <Route path="user/:id" element={<UserUsersPage/>}/>
+                        <Route path="user/new" element={<UserUsersPage/>}/>
                     </Route>
-                    <Route path="/profile" exact={true} element={<AdminLayout/>}>
-                        <Route index element={<ProfilePage/>}/>
+                    <Route path="schools">
+                        <Route index element={<SchoolsPage/>}/>
+                        <Route path=":id" element={<SchoolPage/>}/>
+                        <Route path="new" element={<SchoolPage/>}/>
                     </Route>
-                    <Route path="/login" exact={true} element={<Navigate to="/admin/users"/>}/>
-                    <Route path="/" exact={true} element={<Navigate to="/admin/users"/>}/>
-                    <Route path="*" element={<Page404/>}/>
-                </Routes>
-            </Suspense>
+                    <Route path="theatres">
+                        <Route index element={<TheatresPage/>}/>
+                        <Route path=":id" element={<TheatrePage/>}/>
+                        <Route path="new" element={<TheatrePage/>}/>
+                    </Route>
+                    <Route path="theatreRequests">
+                        <Route index element={<TheatreRequestsPage/>}/>
+                        <Route path=":id" element={<TheatreRequestPage/>}/>
+                        <Route path="new" element={<TheatreRequestPage/>}/>
+                    </Route>
+                    <Route path="teachers">
+                        <Route index element={<AdminTeachersPage/>}/>
+                        <Route path=":id" element={<AdminTeacherPage/>}/>
+                        <Route path="new" element={<AdminTeacherPage/>}/>
+                    </Route>
+                </Route>
+                <Route path="/profile" exact={true} element={<AdminLayout/>}>
+                    <Route index element={<ProfilePage/>}/>
+                </Route>
+                <Route path="/login" exact={true} element={<Navigate to="/admin/users"/>}/>
+                <Route path="/" exact={true} element={<Navigate to="/admin/users"/>}/>
+                <Route path="*" element={<Page404/>}/>
+            </Routes>
         );
 
     }
@@ -86,47 +79,43 @@ const RoutesList = () => {
     if (user && user.role === "user") {
 
         return (
-            <Suspense fallback={<Preloader/>}>
-                <Routes>
-                    <Route path="/user" element={<UserLayout/>}>
-                        <Route path="my_school" index element={<MySchoolPage/>}/>
-                        <Route path="theatres">
-                            <Route index element={<MyTheatresPage/>}/>
-                            <Route path=":id" element={<MyTheatrePage/>}/>
-                            <Route path="new" element={<MyTheatrePage/>}/>
-                        </Route>
-                        <Route path="theatreRequests">
-                            <Route index element={<MyTheatreRequestsPage/>}/>
-                            <Route path=":id" element={<MyTheatreRequestPage/>}/>
-                            <Route path="new" element={<MyTheatreRequestPage/>}/>
-                        </Route>
-                        <Route path="teachers">
-                            <Route index element={<UserTeachersPage/>}/>
-                            <Route path=":id" element={<UserTeacherPage/>}/>
-                            <Route path="new" element={<UserTeacherPage/>}/>
-                        </Route>
+            <Routes>
+                <Route path="/user" element={<UserLayout/>}>
+                    <Route path="my_school" index element={<MySchoolPage/>}/>
+                    <Route path="theatres">
+                        <Route index element={<MyTheatresPage/>}/>
+                        <Route path=":id" element={<MyTheatrePage/>}/>
+                        <Route path="new" element={<MyTheatrePage/>}/>
                     </Route>
-                    <Route path="/profile" exact={true} element={<UserLayout/>}>
-                        <Route index element={<ProfilePage/>}/>
+                    <Route path="theatreRequests">
+                        <Route index element={<MyTheatreRequestsPage/>}/>
+                        <Route path=":id" element={<MyTheatreRequestPage/>}/>
+                        <Route path="new" element={<MyTheatreRequestPage/>}/>
                     </Route>
+                    <Route path="teachers">
+                        <Route index element={<UserTeachersPage/>}/>
+                        <Route path=":id" element={<UserTeacherPage/>}/>
+                        <Route path="new" element={<UserTeacherPage/>}/>
+                    </Route>
+                </Route>
+                <Route path="/profile" exact={true} element={<UserLayout/>}>
+                    <Route index element={<ProfilePage/>}/>
+                </Route>
 
-                    <Route path="/login" exact={true} element={<Navigate to="/user/my_school"/>}/>
-                    <Route path="/" exact={true} element={<Navigate to="/user/my_school"/>}/>
-                    <Route path="*" element={<Page404/>}/>
-                </Routes>
-            </Suspense>
+                <Route path="/login" exact={true} element={<Navigate to="/user/my_school"/>}/>
+                <Route path="/" exact={true} element={<Navigate to="/user/my_school"/>}/>
+                <Route path="*" element={<Page404/>}/>
+            </Routes>
         );
 
     }
 
     return (
-        <Suspense fallback={<Preloader/>}>
-            <Routes>
-                <Route path="/login" exact={true} element={<LoginPage/>}/>
-                <Route path="/" exact={true} element={<Navigate to="/login"/>}/>
-                <Route path="*" element={<Navigate to="/login"/>}/>
-            </Routes>
-        </Suspense>
+        <Routes>
+            <Route path="/login" exact={true} element={<LoginPage/>}/>
+            <Route path="/" exact={true} element={<Navigate to="/login"/>}/>
+            <Route path="*" element={<Navigate to="/login"/>}/>
+        </Routes>
     );
 
 };
