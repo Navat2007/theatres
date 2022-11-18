b79475658e7486b39c7f0d2844bf1d2ffe63050f
Navat2007 <navat2007@yandex.ru>
2022-11-18 19:32:37 +0300

    add

--- public/php/models/user/theatres/add_request.php
+++ public/php/models/user/theatres/add_request.php
@@ -25,13 +25,37 @@ $error_text = "";
 $sqls = array();
 $params = null;
 
-$sql = "SELECT * FROM theatres WHERE title = '$title' AND schoolID = '$schoolID' AND archive = 0";
-$sqls[] = $sql;
-$result = mysqli_query($conn, $sql);
+if ((int)$theatreID !== 0) {
 
-if (mysqli_num_rows($result) > 0) {
-    $error = 1;
-    $error_text = "Театр с таким названием уже существует в данной школе";
+    $sql = "SELECT * FROM theatres WHERE ID = '$theatreID'";
+    $sqls[] = $sql;
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+
+        $row = mysqli_fetch_object($result);
+
+        if ($row->title != $title) {
+            $sql = "SELECT * FROM theatres WHERE title = '$title' AND schoolID = '$schoolID' AND archive = 0";
+            $sqls[] = $sql;
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                $error = 1;
+                $error_text = "Театр с таким названием уже существует в данной школе";
+            }
+        }
+    }
+}
+else{
+    $sql = "SELECT * FROM theatres WHERE title = '$title' AND schoolID = '$schoolID' AND archive = 0";
+    $sqls[] = $sql;
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $error = 1;
+        $error_text = "Театр с таким названием уже существует в данной школе";
+    }
 }
 
 $sql = "SELECT * FROM theatre_requests WHERE title = '$title' AND schoolID = '$schoolID' AND status <> '5' AND archive = 0";
