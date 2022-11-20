843ac1b5abd8009f7d17a74c28d319350ef08c8b
Savallera <phenixvn1985@gmail.com>
2022-11-20 13:39:14 +0300

    +

--- src/components/slider/slider.component.js
+++ src/components/slider/slider.component.js
@@ -1,20 +1,19 @@
 import React from "react";
-import {Carousel} from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Slider = function ({
-                             items = [],
-                             infiniteLoop = false,
-                             swipeable = true,
-                             showStatus = false,
-                             showThumbs = false,
-                             centerMode = false,
-                             width,
-                             height,
-                             autoPlay = false,
-                             autoPlayTime = 5000
-                         }) {
-
+    items = [],
+    infiniteLoop = false,
+    swipeable = true,
+    showStatus = false,
+    showThumbs = false,
+    centerMode = false,
+    width,
+    height,
+    autoPlay = false,
+    autoPlayTime = 5000,
+}) {
     return (
         <Carousel
             autoPlay={autoPlay}
@@ -25,16 +24,17 @@ const Slider = function ({
             showThumbs={showThumbs}
             centerMode={centerMode}
         >
-            {
-                items.length && items.map(item => (
+            {items.length &&
+                items.map((item) => (
                     <div key={item.alt}>
-                        <img src={item.url} alt={item.alt}/>
+                        <img
+                            src={item.url}
+                            alt={item.alt}
+                        />
                     </div>
-                ))
-            }
+                ))}
         </Carousel>
     );
-
 };
 
 export default Slider;
