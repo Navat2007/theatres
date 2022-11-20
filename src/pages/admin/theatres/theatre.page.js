52a3ab1e89dccf98906d8724926bc83e477b46d7
Savallera <phenixvn1985@gmail.com>
2022-11-20 17:24:20 +0300

    add

--- src/pages/admin/theatres/theatre.page.js
+++ src/pages/admin/theatres/theatre.page.js
@@ -1,33 +1,52 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 
-import Button from '../../../components/simple/button/button.component';
+import Button from "../../../components/simple/button/button.component";
 
 const TheatrePage = () => {
-
     let { id } = useParams();
 
     return (
-        <div className='content__section'>
-            {
-                !id && <>
+        <div className="content__section">
+            {!id && (
+                <>
                     <div className="content__title-block">
                         <Button
-                            type='button'
-                            theme='text'
-                            size='small'
-                            iconClass={'mdi mdi-arrow-left'}
-                            isIconBtn='true'
-                            aria-label='Назад'
+                            type="button"
+                            theme="text"
+                            size="small"
+                            iconClass={"mdi mdi-arrow-left"}
+                            isIconBtn="true"
+                            aria-label="Назад"
                         />
-                        <h1 className='content__title'>Создание театра</h1>
+                        <h1 className="content__title">Создание театра</h1>
                     </div>
-
                 </>
-            }
-            {
-                id && <p>Театр №{id}</p>
-            }
+            )}
+            {id && (
+                <>
+                    <div className="content__title-block">
+                        <Button
+                            type="button"
+                            theme="text"
+                            size="small"
+                            iconClass={"mdi mdi-arrow-left"}
+                            isIconBtn="true"
+                            aria-label="Назад"
+                        />
+                        <h1 className="content__title">Театр №-{id}</h1>
+                    </div>
+                    <a
+                        className="link"
+                        href=""
+                        target={"_blank"}
+                        rel="noopener nofollow noreferer"
+                    >
+                        Страница театра{" "}
+                        <span className="mdi mdi-open-in-new"></span>
+                    </a>
+                </>
+            )}
         </div>
     );
 };
