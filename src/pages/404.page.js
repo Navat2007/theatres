74eb9d975cd78f9696111757575d7688eaebc70e
Savallera <phenixvn1985@gmail.com>
2022-11-22 14:48:18 +0300

    error page

--- src/pages/404.page.js
+++ src/pages/404.page.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styles from "./404.page.module.scss";
 
 const Page404 = () => {
     React.useEffect(() => {
@@ -7,31 +8,31 @@ const Page404 = () => {
     }, []);
 
     return (
-        <div className="error-page">
-            <h1 className="error-page__title">
-                <span className="error-page__span-accent">Ошибка 404!</span>
+        <div className={styles.error_page}>
+            <h1 className={styles.title}>
+                <span className={styles.span_accent}>Ошибка 404!</span>
                 Страница не найдена.
             </h1>
-            <div className="error-page__circle --big"></div>
-            <div className="error-page__circle --big-two"></div>
-            <div className="error-page__circle --first"></div>
-            <div className="error-page__circle --second"></div>
-            <div className="error-page__circle --third"></div>
-            <div className="error-page__circle --forth"></div>
-            <div className="error-page__circle --five"></div>
-            <div className="error-page__circle --six"></div>
-            <p className="error-page__text">
+            <div className={styles.circle + ` ` + styles.circle_big}></div>
+            <div className={styles.circle + ` ` + styles.circle_big_two}></div>
+            <div className={styles.circle + ` ` + styles.circle_first}></div>
+            <div className={styles.circle + ` ` + styles.circle_second}></div>
+            <div className={styles.circle + ` ` + styles.circle_third}></div>
+            <div className={styles.circle + ` ` + styles.circle_fourth}></div>
+            <div className={styles.circle + ` ` + styles.circle_five}></div>
+            <div className={styles.circle + ` ` + styles.circle_six}></div>
+            <p className={styles.text}>
                 К сожалению, запрашиваемая Вами страница, не найдена.
                 <br />
                 Повторите попытку позже.
                 <br />
                 <br />
-                Вернуться{" "}
                 <NavLink
                     className="link"
                     to={"/"}
                 >
-                    на главную
+                    Вернуться на главную{" "}
+                    <span className="mdi mdi-open-in-new"></span>
                 </NavLink>
             </p>
         </div>
