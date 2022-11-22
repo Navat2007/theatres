24fce0ce9eb20e9710288728f92b764cda283a4a
Savallera <phenixvn1985@gmail.com>
2022-11-22 12:06:32 +0300

    error page

--- src/pages/404.page.js
+++ src/pages/404.page.js
@@ -1,32 +1,40 @@
-import React from 'react';
-
-import image from '../images/404.png';
+import React from "react";
 
 const Page404 = () => {
-
     React.useEffect(() => {
-
         document.title = "404";
-
     }, []);
 
     return (
         <div className="error-page">
-            <img className='error-page__img' src={image} alt="Ошибка 404" />
-            <h1 className='error-page__title'>Страница не найдена.</h1>
-            <p className='error-page__text'>
+            <h1 className="error-page__title">
+                <span className="error-page__span-accent">Ошибка 404!</span>
+                Страница не найдена.
+            </h1>
+            <div className="error-page__circle --big"></div>
+            <div className="error-page__circle --big-two"></div>
+            <div className="error-page__circle --first"></div>
+            <div className="error-page__circle --second"></div>
+            <div className="error-page__circle --third"></div>
+            <div className="error-page__circle --forth"></div>
+            <div className="error-page__circle --five"></div>
+            <div className="error-page__circle --six"></div>
+            <p className="error-page__text">
                 К сожалению, запрашиваемая Вами страница, не найдена.
                 <br />
                 Повторите попытку позже.
                 <br />
                 <br />
-
-                Вернуться <a className='link' href="./">на главную</a>
+                Вернуться{" "}
+                <a
+                    className="link"
+                    href="./"
+                >
+                    на главную
+                </a>
             </p>
         </div>
-    )
-
-}
+    );
+};
 
 export default Page404;
-
