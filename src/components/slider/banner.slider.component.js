ad6772ace19d8505e4707a27c79279860b8ee5c8
Savallera <phenixvn1985@gmail.com>
2022-12-04 15:03:16 +0300

    верстка главной страницы фронта

--- src/components/slider/banner.slider.component.js
+++ src/components/slider/banner.slider.component.js
@@ -1,57 +1,52 @@
-import React from 'react';
-import {Carousel} from "react-responsive-carousel";
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+import styles from "./banner.module.scss";
+
 const BannerSlider = ({
-                         items,
-                         index = 0,
-                         infiniteLoop = true,
-                         showArrows = true,
-                         showStatus = false,
-                         showThumbs = false,
-                         autoPlay = false,
-                         interval = 10000,
-                         transitionTime = 0,
-                         swipe = true,
-                     }) => {
+    items,
+    index = 0,
+    infiniteLoop = true,
+    showArrows = true,
+    showStatus = false,
+    showThumbs = false,
+    autoPlay = false,
+    interval = 10000,
+    transitionTime = 0,
+    swipe = true,
+}) => {
     return (
         <>
-            <Carousel
-                selectedItem={index}
-                infiniteLoop={infiniteLoop}
-                // showIndicators={false}
-                showArrows={showArrows}
-                showStatus={showStatus}
-                autoPlay={autoPlay}
-                animationHandler={"fade"}
-                interval={interval}
-                transitionTime={transitionTime}
-                stopOnHover
-                swipeable={swipe}
-                emulateTouch={swipe}
-                swipeScrollTolerance={5}
-                showThumbs={showThumbs}
-            >
-                {
-                    items.map((item, index) => (
-                        <div key={index} className="banner">
-                            <div className="banner__wrap">
-                                <img
-                                    className="banner__img"
-                                    src={item.url}
-                                    alt=""
-                                />
-                                <h2 className="banner__title">
-                                    {item.title}
-                                </h2>
-                                <p className="banner__subtitle">
-                                    {item.text}
-                                </p>
-                            </div>
-                        </div>
-                    ))
-                }
-            </Carousel>
+            <section className={styles.section}>
+                <div className={styles.wrap}>
+                    <Carousel
+                        selectedItem={index}
+                        infiniteLoop={infiniteLoop}
+                        // showIndicators={false}
+                        showArrows={showArrows}
+                        showStatus={showStatus}
+                        autoPlay={autoPlay}
+                        animationHandler={"fade"}
+                        interval={interval}
+                        transitionTime={transitionTime}
+                        stopOnHover
+                        swipeable={swipe}
+                        emulateTouch={swipe}
+                        swipeScrollTolerance={5}
+                        showThumbs={showThumbs}
+                    >
+                        {items.map((item, index) => (
+                            <img
+                                key={index}
+                                className={styles.img}
+                                src={item.url}
+                                alt={item.url}
+                            />
+                        ))}
+                    </Carousel>
+                </div>
+            </section>
         </>
     );
 };
--- src/pages/public/index.page.jsx
+++ src/pages/public/index.page.jsx
@@ -1,13 +1,89 @@
-import React from 'react';
-import commonStyles from "../common.module.scss";
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerSlider from "../../components/slider/banner.slider.component";
+import commonStyles from "./common.module.scss";
 
 const MainPage = () => {
     return (
         <>
+            <BannerSlider
+                transitionTime={1000}
+                autoPlay={true}
+                swipe={false}
+                showArrows={false}
+                items={[
+                    {
+                        url: "./images/banners/banner_1.jpg",
+                    },
+                    {
+                        url: "./images/banners/banner_2.jpg",
+                    },
+                    {
+                        url: "./images/banners/banner_3.jpg",
+                    },
+                ]}
+            />
             <section className={commonStyles.section}>
-                <article className={commonStyles.wrap}>
-                    <h1 className={commonStyles.title}>О Содружестве</h1>
-                </article>
+                <div
+                    className={[
+                        commonStyles.wrap,
+                        commonStyles.twoColumns,
+                    ].join(" ")}
+                >
+                    <article className={commonStyles.concord}>
+                        <h1 className={commonStyles.title}>О Содружестве</h1>
+                        <img
+                            className={commonStyles.concordImg}
+                            src="https://kprf.ru/m/900/700/t/img/2016/09/713431_ssi_8962_novyi-razmer.jpg"
+                            alt="https://kprf.ru/m/900/700/t/img/2016/09/713431_ssi_8962_novyi-razmer.jpg"
+                        />
+                        <div className={commonStyles.concordMainText}>
+                            <p>
+                                <b>Дорогие друзья!</b> <br />
+                                Мы рады приветствовать вас на портале
+                                «Содружество школьных театров города Москвы».
+                            </p>
+                            <p>
+                                Здесь вы можете познакомиться с деятельностью
+                                детских и молодежных театров на базе
+                                образовательных организаций города Москвы, стать
+                                участником или зрителем открытых показов в
+                                рамках ежегодного фестиваля-конкурса «Живая
+                                сцена» и узнать о проектах Содружества.
+                            </p>
+                        </div>
+                        <Link className={commonStyles.linkButton}>
+                            Подробнее
+                        </Link>
+                    </article>
+                    <asside className={commonStyles.news}>
+                        <h2 className={commonStyles.title}>Новости</h2>
+                        <ul className={commonStyles.newsList}>
+                            <li>
+                                <img
+                                    className={commonStyles.newImg}
+                                    src="https://kprf.ru/m/900/700/t/img/2016/09/713431_ssi_8962_novyi-razmer.jpg"
+                                    alt="https://kprf.ru/m/900/700/t/img/2016/09/713431_ssi_8962_novyi-razmer.jpg"
+                                />
+                                <p className={commonStyles.newDate}>
+                                    15.11.2022г.
+                                </p>
+                                <h3 className={commonStyles.newTitle}>
+                                    Название новости
+                                </h3>
+                                <p className={commonStyles.newPharagraph}>
+                                    На прошлой неделе в школе прошел школьный
+                                    конкурс «Театральные подмостки». В нем
+                                    приняли участие многие ученики нашей
+                                    огромной
+                                </p>
+                            </li>
+                        </ul>
+                        <Link className={commonStyles.linkButton}>
+                            Все новости
+                        </Link>
+                    </asside>
+                </div>
             </section>
             <section
                 className={[
