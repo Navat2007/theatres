c7000f5078c150f139c0175f1069fcce0120f0e7
Navat2007 <navat2007@yandex.ru>
2022-12-05 15:46:47 +0300

    add

--- src/components/reach_editor/editor.component.js
+++ src/components/reach_editor/editor.component.js
@@ -2,14 +2,20 @@ import React from 'react';
 import JoditEditor from "jodit-react";
 import { Controller } from "react-hook-form";
 
-// all options from https://xdsoft.net/jodit/doc/,
+// all options from https://xdsoft.net/jodit/docs/,
 const Editor = ({ control, name, readonly, placeholder, required, value, ...rest }) => {
 
     const config = {
         readonly: readonly,
+        toolbarSticky: false,
         about: false,
         addNewLine: false,
+        showCharsCounter: false,
+        showWordsCounter: false,
+        showXPathInStatusbar: false,
         minHeight: '500px',
+        buttons: "bold,italic,underline,ul,ol,font,fontsize,align,|,image,video,|,undo,redo,fullsize,preview,print",
+        //preset: 'inline',
         placeholder: placeholder || 'Начните писать...'
     };
 
