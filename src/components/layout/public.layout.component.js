4c555233ac31288b9768ac654a9f47ba9b6a84a7
Savallera <phenixvn1985@gmail.com>
2022-12-05 14:29:22 +0300

    паблик футер

--- src/components/layout/public.layout.component.js
+++ src/components/layout/public.layout.component.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link, NavLink, Outlet} from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import styles from "./public.module.scss";
 import logo from "../../images/logo.png";
 
@@ -8,36 +8,42 @@ const PublicLayout = () => {
         <>
             <div className={styles.content}>
                 <header className={styles.header}>
-                    <div className={styles.header_wrap}>
+                    <div className={styles.headerWrap}>
                         <img
-                            className={styles.header_logo}
+                            className={styles.headerLogo}
                             src={logo}
                             alt="Логотип Содружества"
                         />
                         <nav className={styles.menu}>
-                            <ul className={styles.menu_list}>
+                            <ul className={styles.menuList}>
                                 <li>
-                                    <NavLink to={"/concord/"} className={styles.menu_link}>
+                                    <NavLink
+                                        to={"/concord/"}
+                                        className={styles.menuLink}
+                                    >
                                         О Содружестве
                                     </NavLink>
                                 </li>
                                 <li>
-                                    <Link className={styles.menu_link}>
+                                    <Link className={styles.menuLink}>
                                         Деятельность
                                     </Link>
                                 </li>
                                 <li>
-                                    <NavLink to={"/theatre/"} className={styles.menu_link}>
+                                    <NavLink
+                                        to={"/theatre/"}
+                                        className={styles.menuLink}
+                                    >
                                         Театры
                                     </NavLink>
                                 </li>
                                 <li>
-                                    <Link className={styles.menu_link}>
+                                    <Link className={styles.menuLink}>
                                         Фестиваль - конкурс «Живая сцена»
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link className={styles.menu_link}>
+                                    <Link className={styles.menuLink}>
                                         Новости
                                     </Link>
                                 </li>
@@ -49,36 +55,59 @@ const PublicLayout = () => {
                     <Outlet />
                 </main>
                 <footer className={styles.footer}>
-                    <div className={styles.footer_wrap}>
-                        <nav className={styles.menu}>
-                            <ul className={styles.menu_list}>
-                                <li>
-                                    <Link className={styles.menu_link}>
-                                        О Содружестве
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link className={styles.menu_link}>
-                                        Деятельность
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link className={styles.menu_link}>
-                                        Театры
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link className={styles.menu_link}>
-                                        Фестиваль - конкурс «Живая сцена»
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link className={styles.menu_link}>
-                                        Новости
-                                    </Link>
-                                </li>
-                            </ul>
-                        </nav>
+                    <div className={styles.footerWrap}>
+                        <div className={styles.footerColumns}>
+                            <div className={styles.footerColumn}>
+                                <h2 className={styles.footerColumnHeading}>
+                                    Контакты
+                                </h2>
+                            </div>
+                            <div className={styles.footerColumn}>
+                                <h2 className={styles.footerColumnHeading}>
+                                    Меню
+                                </h2>
+                                <nav
+                                    className={[
+                                        styles.menu,
+                                        styles.menuPlaceFooter,
+                                    ].join(" ")}
+                                >
+                                    <ul className={styles.menuList}>
+                                        <li>
+                                            <Link className={styles.menuLink}>
+                                                О Содружестве
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link className={styles.menuLink}>
+                                                Деятельность
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link className={styles.menuLink}>
+                                                Театры
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link className={styles.menuLink}>
+                                                Фестиваль - конкурс «Живая
+                                                сцена»
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link className={styles.menuLink}>
+                                                Новости
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            <div className={styles.footerColumn}>
+                                <h2 className={styles.footerColumnHeading}>
+                                    Соцсети
+                                </h2>
+                            </div>
+                        </div>
                     </div>
                 </footer>
             </div>
