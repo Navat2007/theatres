4559407e69ebd40bfe7d75aaf95cb0190084893c
Savallera <phenixvn1985@gmail.com>
2022-12-13 12:00:32 +0300

    верстка страницы Все новости

--- src/pages/public/news/all.news.page.jsx
+++ src/pages/public/news/all.news.page.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+import styles from "./news.module.scss";
 
 const AllNewsPage = () => {
     return (
-        <div>
-            Новости
-        </div>
+        <>
+            <section className={styles.section}>
+                <article className={styles.wrap}></article>
+            </section>
+        </>
     );
 };
 
