2cdc967a6691c945e184de1f143ac44d4c93c46a
Navat2007 <navat2007@yandex.ru>
2022-12-14 11:35:14 +0300

    add

--- src/pages/public/news/all.news.page.jsx
+++ src/pages/public/news/all.news.page.jsx
@@ -4,7 +4,16 @@ import styles from "./news.module.scss";
 const AllNewsPage = () => {
     return (
         <section className={styles.section}>
-            <article className={styles.wrap}></article>
+            <h2>Новости</h2>
+            <iframe
+                seamless={true}
+                title={"Новости"}
+                width={"100%"}
+                height={800}
+                style={{ border: 0 }}
+                //src={"https://patriotsport.moscow/premery-spektaklej/"}
+                src={"https://razgovor.moscow/test.html"}
+            />
         </section>
     );
 };
--- src/pages/public/theatres/theatres.page.jsx
+++ src/pages/public/theatres/theatres.page.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 const TheatresPage = () => {
     return (
-        <div>
-            Театры
-        </div>
+        <section>
+            <h2>Театры</h2>
+            <iframe
+                seamless={true}
+                title={"Театры"}
+                width={"100%"}
+                height={800}
+                style={{ border: 0 }}
+                //src={"https://patriotsport.moscow/premery-spektaklej/"}
+                src={"https://razgovor.moscow/test.html"}
+            />
+        </section>
     );
 };
 
